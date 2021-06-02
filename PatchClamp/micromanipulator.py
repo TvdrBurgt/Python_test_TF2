a8904dcddff247308e3cd297afc5c6d3078957a9
TvdrBurgt <t.h.j.vanderburgt@gmail.com>
2021-06-02 17:15:53 +0200

    Revision of the micromanipulator where the patchstar is a serial object.

--- PatchClamp/micromanipulator.py
+++ PatchClamp/micromanipulator.py
@@ -1,46 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 16 16:56:57 2021
+Created on Wed Jun  2 12:58:59 2021
 
 @author: tvdrb
 """
 
 import serial
 import numpy as np
+import time
 
 
-class ScientificaPatchStar:
-    """
+class ScientificaPatchStar(serial.Serial):
+    """ Serial instance for Scientifica PatchStar control
     This class is for controlling the Scientifica PatchStar micromanipulator.
     The class features a built-in rotation matrix for aligning the coordinate
     system of the PatchStar with the camera field-of-fiew as reference.
     """
-    def __init__(self, address='COM16', baudrate=38400):
-        # Serial settings
-        self.port = address
-        self.baudrate = baudrate
-        self.CRending = '\r'
+    def __init__(self, address='COM16', baud=38400):
+        # Set up serial connection
+        super().__init__(port=self.address, baudrate=self.baud, timeout=1)
         
-        # Rotation matrix for camera FOV alignment
+        # Initiate default settings
+        self.CRending = '\r'                    # Carriage return
+        self.reference = np.array([0, 0, 0])    # x, y, z (in um)
+        self.position = np.array([0, 0, 0])     # x, y, z (in um)
+        self.R = np.identity(3)                 # 3D rotation matrix
+        self.Rinv = np.identity(3)              # 3D rotation matrix inverse
+        
+        # Fill-in known settings
+        self.reference = self.getPos()
+        self.position = self.getPos()
         [self.R, self.Rinv] = self.constructrotationmatrix()
-        
-        # try out connection
-        command = 'DESC' + self.CRending
-        with serial.Serial(self.port, self.baudrate, timeout=3) as patchstar:
-            # Encode the command to ascii and send to PatchStar
-            patchstar.write(command.encode('ascii'))
-            # Wait until all data is written
-            patchstar.flush()
-            # Read PatchStar response until carriage return
-            response = patchstar.read_until(self.CRending.encode('ascii'))
-            # Decodes response to utf-8
-            response = response.decode('utf-8')
-            # Strip off the carriage return
-            response = response.rstrip(self.CRending)
-        if response == 'PatchStar':
-            print('found: 1 {}'.format(response))
-        else:
-            print('No PatchStar found')
         
     @staticmethod
     def constructrotationmatrix(alpha=0, beta=0, gamma=0):
@@ -59,60 +49,41 @@ class ScientificaPatchStar:
                             [-np.sin(gamma), np.cos(gamma), 0],
                             [0, 0, 1]])
         
-        # Full rotation matrix
+        # Full rotation matrix and its inverse
         R = R_gamma @ R_beta @ R_alpha
-        
-        # Inverse of rotation matrix
         Rinv = np.transpose(R)
         
         return R, Rinv
     
-    
-    def stop(self):
+    def wait_until_finished(self):
         """
-        Stops any motion.
-        """
-        command = 'STOP' + self.CRending
-        
-        with serial.Serial(self.port, self.baudrate) as patchstar:
-            # Encode the command to ascii and send to PatchStar
-            patchstar.write(command.encode('ascii'))
-            # Wait until all data is written
-            patchstar.flush()
-            # Read PatchStar response until carriage return
-            response = patchstar.read_until(self.CRending.encode('ascii'))
-            # Decodes response to utf-8
-            response = response.decode('utf-8')
-            # Strip off the carriage return
-            response = response.rstrip(self.CRending)
-            
-        return response
-    
-    def reportStatus(self):
-        """
-        Returns the status of the patchstar, responses:
-            0 - Motors idle.
-            1 - Start speed (point to point moves)
-            2 - Accelaration (point to point moves)
-            3 - Max speed (point to point moves)
-            4 - Deceleration (point to point moves)
-            5 - Stopping (point to point moves)
-            6 - Input device moves
-            7 - Constant velocity moves
+        This function checks if motors are idle and updates the instance
+        coordinates afterwards.
+        'S' returns the status of the patchstar, '0' means motors are idle
+        'P' returns the patchstar coordinates, 'x y z' is the position
         """
         command = 'S' + self.CRending
         
-        with serial.Serial(self.port, self.baudrate) as patchstar:
-            # Encode the command to ascii and send to PatchStar
-            patchstar.write(command.encode('ascii'))
-            # Wait until all data is written
-            patchstar.flush()
-            # Read PatchStar response until carriage return
-            response = patchstar.read_until(self.CRending.encode('ascii'))
-            # Decodes response to utf-8
-            response = response.decode('utf-8')
-            # Strip off the carriage return
-            response = response.rstrip(self.CRending)
+        response = '-1'
+        while True:
+            try:
+                # Encode the command to ascii and send to PatchStar
+                self.write(command.encode('ascii'))
+                # Wait until all data is written
+                self.flush()
+                # Read PatchStar response until carriage return
+                response = self.read_until(self.CRending.encode('ascii'))
+                # Decodes response to utf-8
+                response = response.decode('utf-8')
+                # Strip off the carriage return
+                response = response.rstrip(self.CRending)
+            except:
+                print('Busy traffic')
+                
+            if response != '0':
+                time.sleep(0.05)
+            else:
+                break
             
         return response
     
@@ -124,26 +95,25 @@ class ScientificaPatchStar:
         """
         command = 'P' + self.CRending
         
-        with serial.Serial(self.port, self.baudrate) as patchstar:
-            # Encode the command to ascii and send to PatchStar
-            patchstar.write(command.encode('ascii'))
-            # Wait until all data is written
-            patchstar.flush()
-            # Read PatchStar response until carriage return
-            response = patchstar.read_until(self.CRending.encode('ascii'))
-            # Decodes response to utf-8
-            response = response.decode('utf-8')
-            # Strip off the carriage return
-            response = response.rstrip(self.CRending)
-            
-            # Split response by at the tabs
-            [x, y, z] = response.split('\t')
-            
-            # Convert coordinates to float and apply rotation matrix
-            [x, y, z] = self.R @ [[float(x), float(y), float(z)]]
-            
-        return x, y, z
+        # Encode the command to ascii and send to PatchStar
+        self.write(command.encode('ascii'))
+        # Wait until all data is written
+        self.flush()
+        # Read PatchStar response until carriage return
+        response = self.read_until(self.CRending.encode('ascii'))
+        # Decodes response to utf-8
+        response = response.decode('utf-8')
+        # Strip off the carriage return
+        response = response.rstrip(self.CRending)
         
+        # Split response by at the tabs
+        [x, y, z] = response.split('\t')
+        
+        # Convert coordinates to float and apply rotation matrix
+        [x, y, z] = self.R @ [float(x), float(y), float(z)]
+        
+        return x, y, z
+    
     def moveAbs(self, x, y, z):
         """
         Moves the patchstar to the given absolute position, example:
@@ -155,94 +125,62 @@ class ScientificaPatchStar:
         
         command = "ABS %d %d %d" % (x,y,z) + self.CRending
         
-        with serial.Serial(self.port, self.baudrate) as patchstar:
-            # Encode the command to ascii and send to PatchStar
-            patchstar.write(command.encode('ascii'))
-            # Wait until all data is written
-            patchstar.flush()
-            # Read PatchStar response until carriage return
-            response = patchstar.read_until(self.CRending.encode('ascii'))
-            # Decodes response to utf-8
-            response = response.decode('utf-8')
-            # Strip off the carriage return
-            response = response.rstrip(self.CRending)
+        # Encode the command to ascii and send to PatchStar
+        self.write(command.encode('ascii'))
+        # Wait until all data is written
+        self.flush()
+        # Read PatchStar response until carriage return
+        response = self.read_until(self.CRending.encode('ascii'))
+        # Decodes response to utf-8
+        response = response.decode('utf-8')
+        # Strip off the carriage return
+        response = response.rstrip(self.CRending)
+            
+        # Wait untill move has finished
+        self.wait_until_finished()
             
         return response
     
     def moveAbsZ(self, z):
-        self.moveAbs(0, 0, z)
-    #     """
-    #     Moves the patchstar to the absolute position on the z axis, example:
-    #         Send: ABSZ 128
-    #         Response: A (if move allowed else E)
-    #     """
-        
-    #     command = "ABSZ %d" % z + self.CRending
-        
-    #     with serial.Serial(self.port, self.baudrate) as patchstar:
-    #         # Encode the command to ascii and send to PatchStar
-    #         patchstar.write(command.encode('ascii'))
-    #         # Wait until all data is written
-    #         patchstar.flush()
-    #         # Read PatchStar response until carriage return
-    #         response = patchstar.read_until(self.CRending.encode('ascii'))
-    #         # Decodes response to utf-8
-    #         response = response.decode('utf-8')
-    #         # Strip off the carriage return
-    #         response = response.rstrip(self.CRending)
-            
-    #     return response
+        """
+        Moves the PatchStar to the given absolute position in z. The x and y
+        coordinates do not change.
+        """
+        targetpos = [self.position[0], self.position[1], z]
+        self.moveAbs(targetpos)
         
     def moveRel(self, x=0, y=0, z=0):
         """
-        Moves the patchstar relative to the current position, example:
-            Send: REL 100 26 3
-            Response: A (if move allowed else E) but it is always allowed, it
-            just moves to the edge...
+        Moves the patchstar relative from the initial position. The function
+        calls the absolute movement function - to bypass building up
+        imperfections from relative movements - and calculates the target
+        position by adding the current position.
         """
-        # Apply rotation matrix to input coordinates
-        [x, y, z] = self.Rinv @ [x, y, z]
+        # Make relative move an aboslute move
+        targetpos = self.position + [x, y, z]
         
-        command = "ABS %d %d %d" % (x,y,z) + self.CRending
+        # Move the PatchStar
+        response = self.moveAbs()
         
-        with serial.Serial(self.port, self.baudrate) as patchstar:
-            # Encode the command to ascii and send to PatchStar
-            patchstar.write(command.encode('ascii'))
-            # Wait until all data is written
-            patchstar.flush()
-            # Read PatchStar response until carriage return
-            response = patchstar.read_until(self.CRending.encode('ascii'))
-            # Decodes response to utf-8
-            response = response.decode('utf-8')
-            # Strip off the carriage return
-            response = response.rstrip(self.CRending)
-            
         return response
     
-    def setZero(self):
+    def stop(self):
         """
-        Sets the current position to (0,0,0) as long as the PatchStar is not
-        moving (S command returns 0), example:
-            Send: ZERO
-            Response: A (if set allowed else E)
+        Stops any motion.
         """
-        command = 'ZERO' + self.CRending
+        command = 'STOP' + self.CRending
         
-        with serial.Serial(self.port, self.baudrate) as patchstar:
-            # Encode the command to ascii and send to PatchStar
-            patchstar.write(command.encode('ascii'))
-            # Wait until all data is written
-            patchstar.flush()
-            # Read PatchStar response until carriage return
-            response = patchstar.read_until(self.CRending.encode('ascii'))
-            # Decodes response to utf-8
-            response = response.decode('utf-8')
-            # Strip off the carriage return
-            response = response.rstrip(self.CRending)
+        # Encode the command to ascii and send to PatchStar
+        self.write(command.encode('ascii'))
+        # Wait until all data is written
+        self.flush()
+        # Read PatchStar response until carriage return
+        response = self.read_until(self.CRending.encode('ascii'))
+        # Decodes response to utf-8
+        response = response.decode('utf-8')
+        # Strip off the carriage return
+        response = response.rstrip(self.CRending)
             
         return response
-
-if __name__ == '__main__':
-    manipulator = ScientificaPatchStar('COM16')
     
     
