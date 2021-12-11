e3ad1fe681077e6be5161355df3fc61506b6e1e8
TvdrBurgt <t.h.j.vanderburgt@gmail.com>
2021-12-11 23:25:15 +0100

    Uncommented hardware threads and the application.quit for at home debugging.

--- PatchClamp/smartpatcher_frontend.py
+++ PatchClamp/smartpatcher_frontend.py
@@ -731,7 +731,7 @@ class PatchClampUI(QWidget):
         event.accept()
         
         # Frees the console by quitting the application entirely
-        # QtWidgets.QApplication.quit() # remove when part of Tupolev!!
+        QtWidgets.QApplication.quit() # remove when part of Tupolev!!
         
         
 
